5fe1f386c5128f08ddae64dbf0826994b0cb6362
Zuzanna Godek <[omitted]>
2017-06-16 12:55:33 +0200

    dodano pliki index.html i scripts.js

---
+++ scripts.js
@@ -0,0 +1,18 @@
+//alert('hello');
+
+// var buttons = document.getElementsByClassName('button');
+// console.log(buttons);
+
+// var buttonsContent = buttons.innerHTML;
+// console.log(buttonsContent);
+
+// console.log(document.getElementsByClassName('button').textContent);
+
+// console.log(buttons[0].innerHTML);
+// console.log(buttons[1].innerHTML);
+// console.log(buttons[2].innerHTML);
+// console.log(buttons[3].innerHTML);
+
+for (var i = 0; i < buttons.length; i++) {
+	console.log(buttons[i].innerHTML);
+}
